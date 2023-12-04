69f48025b2f2c3bc2314a3e57eb3d3031c06d8d7
MuriloBarranco <murilo_cesar7@hotmail.com>
2023-12-04 10:53:15 -0300

    feature:challenge

---
+++ src/pages/transactions/index.tsx
@@ -0,0 +1,49 @@
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
+import { SearchForm } from './Components/SearchForm'
+import { useContextSelector } from 'use-context-selector'
+import {
+  PriceHighlight,
+  TransactionsTable,
+  TrasactionsConatiener,
+} from './styles'
+
+export function Transactions() {
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
+
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TrasactionsConatiener>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            {transactions.map((transactions) => {
+              return (
+                <tr key={transactions.id}>
+                  <td width="50%">{transactions.description}</td>
+                  <td>
+                    <PriceHighlight variant={transactions.type}>
+                      {transactions.type === 'outcome' && '- '}
+                      {priceFormatter.format(transactions.price)}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transactions.category}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transactions.createdAt))}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </TransactionsTable>
+      </TrasactionsConatiener>
+    </div>
+  )
+}
